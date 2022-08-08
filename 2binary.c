b32a12105d5971d818d88979e1e6e56932786f12
Scar-Ashley <95442615+Aysha-Fathima@users.noreply.github.com>
2022-08-09 01:40:19 +0530

    recursive updated

--- 2binary.c
+++ 2binary.c
@@ -1,62 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 #define x 10
 #define y 100
 #define incre 10
-
-int binary(int a[],int key,int n)
+int c = 0;
+int binary(int a[],int key,int low,int high)
 {
-    int mid,high = n-1,low = 0,c = 0,i;
-    while(low <= high)
-    {
+    int mid,i;
         mid = (low + high) / 2;
         c++;
-        if(a[mid] == key)
+        if(high < low)
         {
-            return c;
+            return -1;
+        }
+        else if(a[mid] == key)
+        {
+            return mid;
         }
         else if(a[mid] < key)
         {
-            low = mid + 1;
+            binary(a,key,mid+1,high);
         }
         else
         {
-            high = mid -1;
+            binary(a,key,low,mid-1);
         }
-    }
-    return c;
+}
+
+void bs(int a[],int n,int key,char fn[])
+{
+    c = 0;
+    FILE *fp = fopen(fn,"a");
+    binary(a,key,0,n-1);
+    fprintf(fp,"%d\t%d\n",n,c);
 }
 
 void main()
 {
-    int n,*a,c,key,i;
-    FILE *f1,*f2,*f3;
-    system("del bb.txt");
-    system("del ba.txt");
-    system("del bw.txt");
-    for(n = x;n <= y;n += incre)
+    system("del -f b*.txt");
+    srand(time(NULL));
+    for(int n = x;n <= y;n+=incre)
     {
-        a = (int*)malloc(n*sizeof(int));
-        for(i = 0;i < n;i++)
+        int a[n];
+        for(int i = 0;i < n;i++)
         {
-            a[i] = i + 10;
+            a[i] = i+1;
         }
-        key = a[(n-1)/2];
-        f1 = fopen("bb.txt","a");
-        c = binary(a,key,n);
-        fprintf(f1,"%d\t%d\n",n,c);
-        fclose(f1);
-
-        key = a[(n-3)/2]; //generate any random number from array
-        f2 = fopen("ba.txt","a");
-        c = binary(a,key,n);
-        fprintf(f2,"%d\t%d\n",n,c);
-        fclose(f2);
-
-        key = a[0];
-        f3 = fopen("bw.txt","a");
-        c = binary(a,key,n);
-        fprintf(f3,"%d\t%d\n",n,c);
-        fclose(f3);
+        bs(a,n,a[(n-1)/2],"bb.txt");
+        bs(a,n,a[rand()%n],"ba.txt");
+        bs(a,n,999,"bw.txt");
     }
+
 }
