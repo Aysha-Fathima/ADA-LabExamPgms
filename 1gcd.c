e112dfc259004cb09755082a6f7ae451b34d5a3b
Scar-Ashley <95442615+Aysha-Fathima@users.noreply.github.com>
2022-08-08 19:37:41 +0530

    Add files via upload

---
+++ 1gcd.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+void main()
+{
+    int count = 0,m1,n1,m2,n2,min,temp1 = 0,temp2 = 0;
+    printf("Enter two numbers to find gcd\n");
+    scanf("%d%d",&m1,&n1);
+    m2 = m1;
+    n2 = n1;
+    //euclids modified algo based on subtraction
+    while(m1 != n1)
+    {
+        count++;
+        if(m1 > n1)
+        {
+            m1 = m1 - n1;
+        }
+        else
+        {
+            n1 = n1 - m1;
+        }
+    }
+    printf("euclids modified algo based on subtrction\ngcd is %d\noperational count is %d\n",m1,count);
+
+    //euclids algo based on consecutive integer checking
+    count = 0;
+    if(m2 > n2)
+    {
+        min = n2;
+    }
+    else
+    {
+        min = m2;
+    }
+    while(min != 1)
+    {
+        temp1 = m2 % min;
+        count++;
+        if(temp1 == 0)
+        {
+            temp2 = n2 % min;
+            count++;
+            if(temp2 == 0)
+            {
+                break;
+            }
+        }
+        min  = min - 1;
+    }
+    printf("euclids algo based on consecutive integer checking\ngcd is %d\noperational count is %d",min,count);
+}
