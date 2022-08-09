f1182b80059436bea02d46228487e99f01094365
AyshaFathima <95442615+Aysha-Fathima@users.noreply.github.com>
2022-08-10 02:06:13 +0530

    new program

--- 1gcd.c
+++ 1gcd.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 void main()
 {
-    int count = 0,m1,n1,m2,n2,min,temp1 = 0,temp2 = 0;
+    int count = 0,m1,n1,m2,n2,min,temp1 = 0,temp2 = 0,i,j,item;
+    FILE *f1,*f2;
     printf("Enter two numbers to find gcd\n");
     scanf("%d%d",&m1,&n1);
     m2 = m1;
     n2 = n1;
-    //euclids modified algo based on subtraction
+//euclids modified algo based on subtraction
     while(m1 != n1)
     {
         count++;
@@ -19,6 +20,9 @@ void main()
             n1 = n1 - m1;
         }
     }
+    f1=fopen("e1w.txt","a");
+    fprintf(f1,"%d\n",count);
+    fclose(f1);
     printf("euclids modified algo based on subtrction\ngcd is %d\noperational count is %d\n",m1,count);
 
     //euclids algo based on consecutive integer checking
@@ -46,5 +50,8 @@ void main()
         }
         min  = min - 1;
     }
+    f2=fopen("e2w.txt","a");
+    fprintf(f2,"%d\n",count);
+    fclose(f2);
     printf("euclids algo based on consecutive integer checking\ngcd is %d\noperational count is %d",min,count);
 }
