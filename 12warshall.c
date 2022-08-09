801aad4fa0908df0b3d36d1eb9461cee7f58560e
AyshaFathima <95442615+Aysha-Fathima@users.noreply.github.com>
2022-08-10 02:04:06 +0530

    modified

--- 12warshall.c
+++ 12warshall.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 void main()
 {
     int n,cnt = 0;
-    printf("enter the number of vertices\n");
-    scanf("%d",&n);
+    srand(time(NULL));
+    FILE *f1;
+    system("del war_*.txt");
+    for(n = 3;n <= 15;n++)
+    {
     int a[n][n],i,j,k;
-    printf("enter the adjacency matrix\n");
+    f1 = fopen("war_data.txt","a");
+    fprintf(f1,"\nThe adjacency matrix\n");
     for(i = 0;i < n;i++)
     {
         for(j = 0;j < n;j++)
         {
-            scanf("%d",&a[i][j]);
+            a[i][j] = rand()%2;
+            fprintf(f1,"%d\t",a[i][j]);
         }
+        fprintf(f1,"\n");
     }
     for(i = 0;i < n;i++)
     {
@@ -20,6 +27,7 @@ void main()
         {
             for(k = 0;k < n;k++)
             {
+                cnt++;
                 if(a[i][j] == 0 && a[i][k] == 1 && a[k][j] == 1)
                 {
                     a[i][j] = 1;
@@ -27,13 +35,18 @@ void main()
             }
         }
     }
-    printf("\nTransitive closure is\n");
+    fprintf(f1,"\nTransitive closure is\n");
     for(i = 0;i < n;i++)
     {
         for(j = 0;j < n;j++)
         {
-            printf("%d\t",a[i][j]);
+            fprintf(f1,"%d\t",a[i][j]);
         }
-        printf("\n");
+        fprintf(f1,"\n");
     }
+    fclose(f1);
+    f1 = fopen("war_count.txt","a");
+    fprintf(f1,"%d\t%d\n",n,cnt);
+    fclose(f1);
+   }
 }
