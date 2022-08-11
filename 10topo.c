ff3e77f87048d8c9baa2ca77f470414dcdb9616c
AyshaFathima <95442615+Aysha-Fathima@users.noreply.github.com>
2022-08-12 00:04:51 +0530

    unwanted lines removed

--- 10topo.c
+++ 10topo.c
@@ -1,4 +1,3 @@
-///*
 #include<stdio.h>
 #include<stdlib.h>
 void main()
@@ -45,57 +44,15 @@ void main()
             {
                 vis[j] = 1;
                 printf("%d -> ",j);
-            
-            for(k = 0;k < n;k++)
-            {
-                if(a[j][k])
+                for(k = 0;k < n;k++)
                 {
-                    a[j][k] = 0;
-                    in[k]--;
-                }
-            }
-            }
-            
-        }
-    }
-}
-//*/
-/*
-#include<stdio.h>
-#include<stdlib.h>
-
-void main(){
-    int n;
-    printf("Enter the number of nodes>> ");
-    scanf("%d", &n);
-    int graph[n][n];
-    int *indeg = (int *)calloc(sizeof(int), n);
-    int *visited = (int *)calloc(sizeof(int), n);
-
-    printf("Enter the matrix>> ");
-    for(int i = 0; i<n; i++)
-        for(int j = 0; j<n; j++)
-            scanf("%d", &graph[i][j]);
-
-    for(int i = 0; i<n; i++)
-        for(int j = 0; j<n; j++)
-            indeg[i] += graph[j][i];
-
-    printf("Topological Sort order>>\n");
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n; j++){
-            if(!visited[j] && !indeg[j]){
-                visited[j] = 1;
-                printf("%d  -->> ", j);
-            }
-
-            for(int k = 0; k<n; k++){
-                if(graph[j][k]){
-                    graph[j][k] = 0;
-                    indeg[k]--;
-                }
+                    if(a[j][k])
+                    {
+                       a[j][k] = 0;
+                       in[k]--;
+                    }
+                 }
             }
         }
     }
 }
-*/
